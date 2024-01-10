1276bb9446db612003e9a56e2b61fd24e0175564
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-01-11 01:14:29 +0530

    video chat app completed

--- Server/index.js
+++ Server/index.js
@@ -23,4 +23,16 @@ io.on('connection',socket=>{
     socket.on("user:call",({to,offer})=>{
         io.to(to).emit("incomming:call",{from :socket.id,offer})
     })
+
+    socket.on("call:accepted",({to,ans})=>{
+        io.to(to).emit("call:accepted",{from :socket.id,ans})
+    })
+
+    socket.on("peer:nego:needed",({to,offer})=>{
+        io.to(to).emit("peer:nego:needed",{from :socket.id,offer})
+    })
+
+    socket.on("peer:nego:done",({to,ans})=>{
+        io.to(to).emit("peer:nego:final",{from :socket.id,ans})
+    })
 })
